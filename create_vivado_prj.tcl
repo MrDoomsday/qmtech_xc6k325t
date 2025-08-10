79602476cbdf0615af88bd5c23e2d0cad861e68a
RB <belenkov.roman@bk.ru>
2025-08-10 11:38:33 +0300

    dev(prj): модифицирован скрипт сборки
    - добавлена возможность подключать совственные ip-ядра

--- create_vivado_prj.tcl
+++ create_vivado_prj.tcl
@@ -1,6 +1,16 @@
-# Создание block design
-create_project -in_memory -part xc7k325tlffg676-2L
+# Создание block design (без создания проекта)
+create_project -in_memory -force -part xc7k325tlffg676-2L
+
+# подключаем папку с самописными ip_cores
+set script_path [file dirname [file normalize [info script]]]
+set dir_ip_cores "$script_path/ip_cores [get_property ip_repo_paths [current_project]]"
+set_property ip_repo_paths $dir_ip_cores [current_project]
+update_ip_catalog
+
+# запускаем сборку BD
 source top_bd.tcl
+
+# делаем обертку ранее сгенерированного BD
 make_wrapper -files [get_files ./bd/top_bd/top_bd.bd] -top
 close_project
 
